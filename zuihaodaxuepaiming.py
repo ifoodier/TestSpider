288088507edb4c051e1c35a295a170d42df99919
ifoodier <78959935@qq.com>
2018-04-20 16:40:10 +0800

    正则表达式

--- zuihaodaxuepaiming.py
+++ zuihaodaxuepaiming.py
@@ -40,21 +40,21 @@ def fillUnivList(ulist, html):
     for tr in soup.find('tbody').children:
         if isinstance(tr, bs4.element.Tag):
             tds = tr('td')  # tds = tr.find_all('td')
-            ulist.append([tds[0].string, tds[1].string, tds[2].string])
+            ulist.append([tds[0].string, tds[1].string, tds[2].string, tds[3].string])
 
 def printUnivList(ulist, num):
-    tplt = "{0:^10}\t{1:{3}^10}\t{2:^10}"
-    print(tplt.format("排名", "学校名称", "省市", chr(12288)))
+    tplt = "{0:^10}\t{1:{4}^10}\t{2:{4}^10}\t{3:^10}"
+    print(tplt.format("排名", "学校名称", "省市", "分数", chr(12288)))
     for i in range(num):
         u = ulist[i]
-        print(tplt.format(u[0], u[1], u[2], chr(12288))) #chr(12288)中文字符空格
+        print(tplt.format(u[0], u[1], u[2], u[3], chr(12288))) #chr(12288)中文字符空格
 
 def main():
     uinfo = []
     url = "http://www.zuihaodaxue.com/zuihaodaxuepaiming2018.html"
     html = getHTMLText(url)
     fillUnivList(uinfo, html)
-    printUnivList(uinfo, 20)
+    printUnivList(uinfo, 100)
 
 main()
 
